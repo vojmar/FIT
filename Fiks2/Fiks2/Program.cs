526931f57d6b02b11407be7910f9a2de3602876f
jaroslavmuller26 <user@DESKTOP-OH70HAC>
2017-09-17 18:15:55 +0200

    Added half of teoretic conditions

--- Fiks2/Fiks2/Program.cs
+++ Fiks2/Fiks2/Program.cs
@@ -32,10 +32,26 @@ namespace Fiks2
             }
             #endregion
 
-            int f_ship = parameters[2];
-            int s_ship = parameters[3];
+
+            int[] f_ship = new int[3];
+            f_ship[0] = parameters[2]; // lenght
+            f_ship[1] = 0;             // x
+            f_ship[2] = 0;             // y
+
+            int[] s_ship = new int[3];
+            s_ship[0] = parameters[3]; //lenght
+            s_ship[1] = 0;             // x
+            s_ship[2] = 1;             // y
 
             #region Processing
+            //TODO: List of conditions (Need add more and check)
+            // ship.lenght < parameters[0] -> (x) - horizontal
+            // if not
+            // ship.lenght > parameters[0] -> (x) - set on vertical -> parameters[1]
+            // left site default ship1[0,0] ship2[0,1]
+            // ship1.lenght > ship2.lenght -> ship2 = moveable first
+            // right site < parameters[0] && parameters[1]
+
             for (int a = 0; a < parameters[0]; a++) //TODO: Move in columns
             {
                 for (int b = 0; b < parameters[1]; b++)
