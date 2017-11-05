653691309df8c699893b6aff00fe296f38c1bb8d
jaroslavmuller26 <user@DESKTOP-OH70HAC>
2017-11-05 18:30:54 +0100

    Start

---
+++ Fiks1_Pokus2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fiks1_Pokus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bitmap = new Bitmap(int width, int height);
+
+
+
+
+
+
+            //TODO: Vyhodnocování černých pixelů
+
+            //Bitmap myImage = (whatever image is in the picturebox)
+            //int count = 0;
+            //for (int y = 0; y < myimage.Height; y++)
+            //{
+            //    for (int x = 0; x < myimage.Width; x++)
+            //    {
+            //        Color c = myimage.GetPixel(x, y);
+            //        if (c.R == 0 && c.G == 0 && c.B == 0)
+            //            count++;
+            //    }
+            //}
+
+        }
+    }
+}
